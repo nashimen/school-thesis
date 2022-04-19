bf4c7b3975ea4ee72745ece1514552a44daa861b
nashimen <nashimen2021@163.com>
2022-04-19 14:53:02 +0800

    test

--- WuJie/asymmetric-effects-of-AP-Covid-19/test.py
+++ WuJie/asymmetric-effects-of-AP-Covid-19/test.py
@@ -1,1 +1,8 @@
 import os, pyltp
+
+from pyltp import SentenceSplitter
+
+sentences = SentenceSplitter.split("早餐很一般，酒店比较老了，是港资企业，插座必须要用转换器才能用，价格的话是周边酒店最便宜的一家了，我们是去办美签，交通比较方便，打车十分钟")
+
+print("\n".join(sentences))
+
